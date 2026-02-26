a63d0b804815104ee3331ddaf6529cabcaf8c870
amrit <amarjeetkaurkhalsa54@gmail.com>
2026-02-26 20:15:38 +0530

    Day 3: Added GCD and Sum Array programs

---
+++ C_problems/Day3_SumArray.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int sumArray(int arr[], int n) {
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+        sum += arr[i];
+    return sum;
+}
+
+int main() {
+    int arr[5], n = 5;
+    printf("Enter 5 numbers: ");
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Sum of array elements: %d\n", sumArray(arr, n));
+    return 0;
+}
