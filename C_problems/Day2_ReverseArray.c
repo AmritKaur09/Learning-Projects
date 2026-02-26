32333bf33f7508b1788f732ff08a1ec8d6bea00c
amrit <amarjeetkaurkhalsa54@gmail.com>
2026-02-26 19:59:27 +0530

    Day 2: Added Reverse Array program

---
+++ C_problems/Day2_ReverseArray.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int n = 5;
+    int temp;
+
+    for(int i = 0; i < n/2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+
+    printf("Reversed array: ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
